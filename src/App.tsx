a517a8419a27547d6add39597bab47bef0ee6fb6
KLdevelop <drapdrop1@gmail.com>
2023-11-03 20:37:00 +0300

    centered

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { AppRoot, SplitLayout, SplitCol, View, Panel, PanelHeader } from '@vkontakte/vkui';
+import { AppRoot, View, Panel, PanelHeader } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import './App.scss';
 
 function App() {
   return (
     <AppRoot>
-      <SplitLayout header={<PanelHeader separator={false} />}>
-        <SplitCol autoSpaced>
-          <View activePanel="main">
-            <Panel id="main">
-              <PanelHeader>VKUI</PanelHeader>
-              Test VK Mini App
-            </Panel>
-          </View>
-        </SplitCol>
-      </SplitLayout>
+      <View activePanel="main">
+        <Panel id="main" centered>
+          <PanelHeader>VKUI</PanelHeader>
+          Test VK Mini App
+        </Panel>
+      </View>
     </AppRoot>
   );
 }
