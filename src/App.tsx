d76da5e72f155ef2951b34477da95e914b27199a
KLdevelop <drapdrop1@gmail.com>
2023-11-07 14:39:54 +0300

    fixed background modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { AppRoot } from '@vkontakte/vkui';
+import { AppRoot, SplitLayout } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import './App.scss';
 import { Navigation } from './Navigation';
@@ -7,8 +7,10 @@ import { AppModalRoot } from './components';
 function App() {
   return (
     <AppRoot>
+      <SplitLayout>
+        <Navigation />
+      </SplitLayout>
       <AppModalRoot />
-      <Navigation />
     </AppRoot>
   );
 }
