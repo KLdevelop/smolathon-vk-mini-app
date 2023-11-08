c866f96fb31b17c16a0766efba5a52b111c20e18
houstonia~ <uwuowo733@gmail.com>
2023-11-08 09:50:37 +0300

    delete cardSchedule import

--- src/tabs/QuestsTab/panels/AboutQuestPanel/AboutContent/AboutContent.tsx
+++ src/tabs/QuestsTab/panels/AboutQuestPanel/AboutContent/AboutContent.tsx
@@ -1,6 +1,6 @@
 import { Div, Title } from '@vkontakte/vkui';
 import './AboutContent.scss';
-import { CardSchedule, DescriptionItem, InfoItem } from '@/components';
+import { DescriptionItem, InfoItem } from '@/components';
 
 interface AboutContentProps {
   questData: QuestData;
@@ -14,7 +14,6 @@ export const AboutContent = ({ questData }: AboutContentProps) => {
         <img src={questData.img} alt="" style={{ width: '100%', height: '100%' }} />
       </Div>
       <DescriptionItem description={questData.description ?? ''} />
-      <CardSchedule />
       <InfoItem />
     </Div>
   );
