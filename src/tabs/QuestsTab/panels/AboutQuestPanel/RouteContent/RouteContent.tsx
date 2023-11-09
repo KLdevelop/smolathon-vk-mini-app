5659b97ec91c9476cde0520163b12aa3c1f0d9ac
KLdevelop <drapdrop1@gmail.com>
2023-11-10 00:14:59 +0300

    route content styles

--- src/tabs/QuestsTab/panels/AboutQuestPanel/RouteContent/RouteContent.tsx
+++ src/tabs/QuestsTab/panels/AboutQuestPanel/RouteContent/RouteContent.tsx
@@ -1,17 +1,17 @@
-import { Group, Div } from '@vkontakte/vkui';
+import { Group, CardGrid } from '@vkontakte/vkui';
 import './RouteContent.scss';
 import { YanMap, CardAttraction } from '@/components';
 
 export const RouteContent = () => {
   return (
-    <Group style={{ marginTop: '10px' }}>
+    <Group className="routeContent">
       <YanMap />
-      <Div style={{ display: 'grid', gap: '20px', justifyContent: 'center' }}>
+      <CardGrid size="l" className="routeContent__attractions">
         <CardAttraction num={1} title="Русская старина" type="Музей" address="ул. Тенишовой 7. Смоленск" />
         <CardAttraction num={2} title="Смоленск - щит России" type="Музей" address="3 Смоленск" />
         <CardAttraction num={3} title="Исторический музей" type="Музей" address="ул. Ленина 8 . Смоленск" />
         <CardAttraction num={4} title="Смоленская крепость" type="Замки" address=" Смоленск" />
-      </Div>
+      </CardGrid>
     </Group>
   );
 };
