f90112233752926f86cb947c1e0f82c1a1404049
KLdevelop <drapdrop1@gmail.com>
2023-11-09 23:16:47 +0300

    fixed types

--- src/components/YanMap/YanMap.tsx
+++ src/components/YanMap/YanMap.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import { GeolocationControl, Map, Placemark, YMaps, ZoomControl, Polyline } from '@pbe/react-yandex-maps';
 import styled from 'styled-components';
 
@@ -8,7 +7,8 @@ const MapContainer = styled.div`
   height: 300px;
   width: 100vw;
 `;
-function calculateMapCenter(coordinates) {
+
+function calculateMapCenter(coordinates: number[][]) {
   let sumLat = 0;
   let sumLng = 0;
 
@@ -19,12 +19,12 @@ function calculateMapCenter(coordinates) {
 
   const centerLat = sumLat / coordinates.length;
   const centerLng = sumLng / coordinates.length;
-console.log([centerLng,centerLat ])
-  return  [centerLat ,centerLng];
+  console.log([centerLng, centerLat]);
+  return [centerLat, centerLng];
 }
 
 export const YanMap = () => {
-  const [mapData, _setMapData] = useState({
+  const [mapData] = useState<ymaps.IMapState>({
     center: calculateMapCenter([
       [54.778416, 32.056272],
       [54.782886, 31.970284],
@@ -32,29 +32,22 @@ export const YanMap = () => {
       [54.78238, 31.958829],
     ]),
     zoom: 13,
-    // controls: ['zoomControl', 'geolocationControl'],
   });
-  const [coordinates, _setCoordinates] = useState<[number, number][]>([
+
+  const [coordinates] = useState<[number, number][]>([
     [54.778416, 32.056272],
     [54.782886, 31.970284],
     [54.781019, 32.032101],
     [54.78238, 31.958829],
   ]);
+
   return (
     <MapContainer>
       <YMaps>
         <div>
           <Map width={'100%'} height={'100%'} defaultState={mapData}>
             {coordinates.map((coordinate, index) => (
-              <Placemark
-                key={`${index}-${coordinate}`}
-                geometry={coordinate}
-                // options={{
-                //   iconLayout: "default#image",
-                //   iconImageSize: [50, 50],
-                //   iconImageHref:` https://img.icons8.com/ios-filled/2x/marker-${'1'.charCodeAt(0)}.png`
-                // }}
-              />
+              <Placemark key={`${index}-${coordinate}`} geometry={coordinate} />
             ))}
             <ZoomControl />
             <GeolocationControl />
@@ -66,7 +59,6 @@ export const YanMap = () => {
                 [54.78238, 31.958829],
               ]}
               options={{
-                balloonCloseButton: false,
                 strokeColor: '#3F8AE0',
                 strokeWidth: 4,
                 strokeOpacity: 0.5,
