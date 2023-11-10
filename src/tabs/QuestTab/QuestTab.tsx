a272b4fc08be0bb3e239150bb7539c5e59f2d451
KLdevelop <drapdrop1@gmail.com>
2023-11-11 00:25:54 +0300

    quest panel

--- src/tabs/QuestTab/QuestTab.tsx
+++ src/tabs/QuestTab/QuestTab.tsx
@@ -1,15 +1,22 @@
 import { View } from '@vkontakte/vkui';
 import { TabProps } from '../TabProps';
 import './QuestTab.scss';
-import { EmptyPanel, QuestPanelIDs } from './panels';
-import { useGetPanelForView } from '@vkontakte/vk-mini-apps-router';
+import { EmptyPanel, QuestPanelID, QuestPanelIDs, QuestPanel } from './panels';
+import { useAppSelector } from '@/hooks';
+import { useEffect, useState } from 'react';
 
 export const QuestTab = ({ id }: TabProps) => {
-  const activePanel = useGetPanelForView();
+  const [activePanel, setActivePanel] = useState<QuestPanelID>(QuestPanelIDs.Quest);
+  const { activeQuest } = useAppSelector((state) => state.activeQuest);
+
+  useEffect(() => {
+    if (!activeQuest) setActivePanel(QuestPanelIDs.Empty);
+  }, [activeQuest]);
 
   return (
-    <View nav={id} activePanel={activePanel ?? QuestPanelIDs.Empty}>
+    <View nav={id} activePanel={activePanel ?? QuestPanelIDs.Quest}>
       <EmptyPanel id={QuestPanelIDs.Empty} />
+      <QuestPanel id={QuestPanelIDs.Quest} activeQuest={activeQuest!} />
     </View>
   );
 };
