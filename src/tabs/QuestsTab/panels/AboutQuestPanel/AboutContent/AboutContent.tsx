e8518ed47cfc735de39e89d040fa78d32ce0c8b6
KLdevelop <drapdrop1@gmail.com>
2023-11-10 17:06:19 +0300

    quest data fetch

--- src/tabs/QuestsTab/panels/AboutQuestPanel/AboutContent/AboutContent.tsx
+++ src/tabs/QuestsTab/panels/AboutQuestPanel/AboutContent/AboutContent.tsx
@@ -1,6 +1,7 @@
 import { Div, Title } from '@vkontakte/vkui';
 import './AboutContent.scss';
 import { DescriptionItem, InfoItem } from '@/components';
+import { routeTypeToString } from '@/utils';
 
 interface AboutContentProps {
   questData: QuestData;
@@ -10,11 +11,19 @@ export const AboutContent = ({ questData }: AboutContentProps) => {
   return (
     <Div style={{ display: 'grid', gap: '20px' }}>
       <Title>{questData.name}</Title>
-      <Div style={{ padding: 0 }}>
-        <img src={questData.img} alt="" style={{ width: '100%', height: '100%' }} />
+      <Div style={{ padding: 0, borderRadius: '8px' }}>
+        <img
+          src={questData.preview.sizes.o.url}
+          alt=""
+          style={{ width: '100%', height: '100%', borderRadius: '8px' }}
+        />
       </Div>
       <DescriptionItem description={questData.description ?? ''} />
-      <InfoItem />
+      <InfoItem
+        duration={`~ ${questData.duration} минут`}
+        reward={`${questData.reward} смолкоинов`}
+        type={routeTypeToString(questData.type)}
+      />
     </Div>
   );
 };
