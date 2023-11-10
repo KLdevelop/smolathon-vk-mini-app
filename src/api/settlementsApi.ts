fad85a63d363a2bd63ca47caa08d629613116d23
KLdevelop <drapdrop1@gmail.com>
2023-11-10 13:49:28 +0300

    fix settlements api

--- src/api/settlementsApi.ts
+++ src/api/settlementsApi.ts
@@ -3,7 +3,7 @@ import { apiUrl } from './apiUrl';
 
 export const settlementsApi = createApi({
   reducerPath: 'settlementsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: apiUrl + '/settlements' }),
+  baseQuery: fetchBaseQuery({ baseUrl: apiUrl + 'settlements' }),
   endpoints: (builder) => ({
     searchSettlements: builder.query<ApiResponse<Settlement[]>, string>({
       query: (query) => ({ url: `search?query=${query}`, method: 'get' }),
