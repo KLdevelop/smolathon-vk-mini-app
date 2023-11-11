d908fbf9de762e6058847ec762ba0f91ce5b35e0
houstonia~ <uwuowo733@gmail.com>
2023-11-11 05:06:30 +0300

    add comps

--- src/tabs/QuestTab/panels/QuestStagePanel/QuestStagePanel.tsx
+++ src/tabs/QuestTab/panels/QuestStagePanel/QuestStagePanel.tsx
@@ -1,11 +1,24 @@
-import { Panel, PanelHeader } from '@vkontakte/vkui';
+import { Button, Div, Panel, PanelHeader, Spacing, Title } from '@vkontakte/vkui';
 import { QuestPanelProps } from '../questPanelProps';
 import { Icon24HelpOutline } from '@vkontakte/icons';
+import { CardTitle,StickyFooter,Task } from '@/components';
 
 export const QuestStagePanel = ({ id }: QuestPanelProps) => {
   return (
     <Panel nav={id}>
       <PanelHeader after={<Icon24HelpOutline />}>Этап квеста</PanelHeader>
+      <Div>
+        <Title level='2'>Исторический музей</Title>
+        <Spacing size={20}/>
+        <CardTitle title='ЗАДАНИЯ'>
+          <Task num={1} status={true}/>
+          <Task num={2} status={false}/>
+          <Task num={3} status={true}/>
+        </CardTitle>
+      </Div>
+      <StickyFooter>
+        <Button size="l" stretched>Введите кодовое слово</Button>
+      </StickyFooter>
     </Panel>
   );
 };
