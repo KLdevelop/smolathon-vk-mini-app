2ece750965fdc1a43a3ddc9837319bc2cae7e588
KLdevelop <drapdrop1@gmail.com>
2023-11-11 09:59:46 +0300

    этап

--- src/tabs/QuestTab/panels/CompletePanel/CompletePanel.tsx
+++ src/tabs/QuestTab/panels/CompletePanel/CompletePanel.tsx
@@ -11,7 +11,7 @@ export const CompletePanel = ({ id }: QuestPanelProps) => {
     <Panel nav={id} centered>
       <PanelHeader separator={false}>Завершение</PanelHeader>
       <Icon28CupOutline width={56} height={56} />
-      <Title>Вы завершили квест</Title>
+      <Title>Вы завершили этап</Title>
       <Text style={{ marginTop: '5px', marginBottom: '7px', textAlign: 'center', padding: '5px' }}>
         Вы успешно прошли битву с боссом и заработали 60 смолкоинов
       </Text>
